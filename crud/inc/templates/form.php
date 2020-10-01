317e2c8e7d6b8256385afeea726416e2d7adb582
coderraseed <coderraseed@gmail.com>
2020-10-01 21:42:12 +0600

    Design part is not complete

---
+++ crud/inc/templates/form.php
@@ -0,0 +1,14 @@
+<form action="/crud/index.php?task=add" method="POST">
+    <label for="svcnum">Svc Number</label>
+    <input type="number" name="svc" id="svcnum" >
+    <label for="name">Name</label>
+    <input type="text" name="name" id="name" >
+    <label for="dname">Designation</label>
+    <input type="text" name="dname" id="dname" >
+    <label for="year">Lenth Of SVC</label>
+    <input type="number" name="lsvc" id="year" >
+    <label for="unit">Unit</label>
+    <input type="text" name="unit" id="unit" >
+    <a href="#" class="button" name='submit' >Submit</a>
+
+</form>
---
+++ crud/inc/templates/nav.php
@@ -0,0 +1,5 @@
+<a class="button" href="">All Employee |</a>
+<a class="button" href="">create |</a>
+<a class="button" href="">update |</a>
+<a class="button" href="">Seeds |</a>
+<a class="button" href="">Delete All |</a>
