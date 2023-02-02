80825c07090611ef5d4929789ab1360f043c9553
Md Rasedul Islam <coderraseed@gmail.com>
2023-02-02 07:15:39 +0600

    This is my First day PHP practice class

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+//echo function
+    echo "Assalamu alaikum \n";
+
+    //01 Feb 2023
+    //variable 
+    $name = "Name";
+    $firstName = "Md Rasedul";
+    $lastName = "Islam";
+    $age    = 32;
+    $country = "banglades";
+    $profession = "PHP & Laravel Trainee";
+
+    echo "$name: $firstName $lastName \n";
+    echo "Age: $age \n";
+    echo "Country Name: $country \n";
+    echo "Profession: $profession";
+    
+    
+
+
+?>
