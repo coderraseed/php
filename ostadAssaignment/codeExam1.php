b11fc6991af99bb99e358d6ba54f9cf0047cc9d6
Md Rasedul Islam <coderraseed@gmail.com>
2023-02-05 23:46:22 +0600

    This is my first live exam code in Ostad

---
+++ ostadAssaignment/codeExam1.php
@@ -0,0 +1,15 @@
+<?php
+$mark =45;
+if($mark>=60){
+    Echo "The grade will be Frist Division";
+  
+}elseif($mark>=45 && $mark<=59){
+        Echo "The grade will be Second Division";
+    
+}elseif($mark>=33 && $mark<=44){
+    Echo "The grade will be Third Division";
+    
+}else{
+    Echo "The student will fail";
+    
+}
