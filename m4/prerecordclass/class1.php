79760d94e7489ddf7f71a7cb59e1522a2b42493f
Md Rasedul Islam <coderraseed@gmail.com>
2023-02-17 15:45:46 +0600

    Prerecorded first class completed

--- m4/prerecordclass/class1.php
+++ m4/prerecordclass/class1.php
@@ -1,1 +1,29 @@
-This is file 1
+<?php
+//$n = 12;//variable
+
+// This is an array
+$student = array(
+    "rahim",
+    "karim",
+    123,
+    "monir",
+);
+var_dump($student);
+echo $student[0];
+echo PHP_EOL;
+
+$n = count($student);
+for ($i=0; $i < $n; $i++) {
+    echo $student[$i] . "\n";
+}
+$trainees = [
+    "Rabin",
+    "Rabiul",
+    "Rabbil",
+    "Ripon",
+];
+$m = count($trainees);
+for ($i=$m-1; $i >=0; $i--) {
+    echo $trainees[$i] . "\n";
+}
+
