0414aab828a77a353408e12d33202e88c9624cf1
Md Rasedul Islam <coderraseed@gmail.com>
2023-02-18 07:57:40 +0600

    I have completed class4 of prerecorder class

--- m4/prerecordclass/class4.php
+++ m4/prerecordclass/class4.php
@@ -1,1 +1,17 @@
-This is file 4
+<?php
+/* $vegetables = "Carrot, Capsicum, Brinjal, Brocolli";
+//explode(); //this function use to convert string to array
+$vegetable = explode(', ', $vegetables);
+var_dump($vegetable);
+echo PHP_EOL;
+//array to string converter
+$vegetableString = join(', ', $vegetable);
+echo $vegetableString;
+echo PHP_EOL; */
+//echo count($vegetable);
+//jadi pasha pashi string a koma na thake ba koma ase but sobguli te nai ai jonno nicher function ti use korte hobe
+//$vege_tables = "Carrot, Capsicum, Brinjal, Brocolli,potato, sweet-potato";
+$vege_tables =preg_split('/(, |,)/' , "Carrot, Capsicum, Brinjal, Brocolli,potato, sweet-potato");
+//$stringToarray = preg_split('/(, |,)/' , $stringToarray);
+echo var_dump($vege_tables);
+
