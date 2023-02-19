650b21aaae5d1f86c7a77a0f682db17b7b0cc8b6
Md Rasedul Islam <coderraseed@gmail.com>
2023-02-19 11:04:01 +0600

    Remove data from an array by that function

--- m4/prerecordclass/class8.php
+++ m4/prerecordclass/class8.php
@@ -1,1 +1,11 @@
-This is file 8
+<?php 
+$student = [
+    'fname' => 'Rafida',
+    'lname' => 'Wasifa',
+
+];
+
+print_r($student);
+unset($student['lname']);
+echo "After removing the data from an array";
+print_r($student);
