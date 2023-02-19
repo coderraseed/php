3be2c69307855659cc01cf776c6c0dc6d46beff4
Md Rasedul Islam <coderraseed@gmail.com>
2023-02-19 08:53:12 +0600

    Class 5 completed

--- m4/prerecordclass/class5.php
+++ m4/prerecordclass/class5.php
@@ -1,1 +1,6 @@
-This is file 5
+<?php
+/* $vegetables = "Carrot, Capsicum, Brinjal, Brocolli";
+//explode(); //this function use to convert string to array
+$vegetable = explode(', ', $vegetables);
+var_dump($vegetable);
+echo PHP_EOL;
