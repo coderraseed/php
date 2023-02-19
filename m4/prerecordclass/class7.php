ddaf0bf343bf5ae6e6008629d4ff3f88ef2275b2
Md Rasedul Islam <coderraseed@gmail.com>
2023-02-19 11:00:02 +0600

    Deep copy and shallo copy of array is done

--- m4/prerecordclass/class7.php
+++ m4/prerecordclass/class7.php
@@ -1,1 +1,63 @@
-This is file 7
+<?php 
+
+//copy by value or deep copy system
+$student = [
+    'fname' => 'Rafida',
+    'lname' => 'Wasifa',
+
+];
+$newstudent = $student;
+
+$newstudent['lname'] = 'Toyeba';
+print_r($student);
+print_r($newstudent);
+
+// copy by reference "&" using this symbol before a variable
+
+$student = [
+    'fname' => 'Rafida',
+    'lname' => 'Wasifa',
+
+];
+$newstudent = &$student;//reference copy means copy the memory place
+$newstudent['lname'] = 'Toiyeba';
+print_r($student);
+print_r($newstudent);
+
+//Shallo copy
+
+$student = [
+    'fname' => 'Rafida',
+    'lname' => 'Wasifa',
+
+];
+$newstudent = &$student;
+$newstudent['lname'] = 'Toiyeba';
+print_r($student);
+print_r($newstudent);
+
+function printData($student){
+    $student['fname'] .= ' Wafia';
+    print_r($student);
+}
+printData($student);
+print_r($student);
+//only function name change "&" symbol position change so out put also change
+//Shallo copy
+
+$student = [
+    'fname' => 'Rafida',
+    'lname' => 'Wasifa',
+
+];
+$newstudent = $student;
+$newstudent['lname'] = 'Toiyeba';
+print_r($student);
+print_r($newstudent);
+
+function printDatar(&$student){
+    $student['fname'] .= ' Wafia';
+    print_r($student);
+}
+printDatar($student);
+print_r($student);
