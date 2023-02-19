2ecd6777309bbbcc85e45881210497f05ff96413
Md Rasedul Islam <coderraseed@gmail.com>
2023-02-19 13:12:44 +0600

    variable empty value discussion

--- m4/prerecordclass/class9.php
+++ m4/prerecordclass/class9.php
@@ -1,1 +1,22 @@
-This is file 9
+<?php 
+//Empty value checked by php function
+$n = 1; //values are "'',0,any name, true, false etc"
+if (isset($name)) {
+    echo "Name is set";
+}else{
+    echo"Name is not set";
+}
+echo PHP_EOL;
+
+if (empty($name)) {
+    echo "Name is empty";
+}else{
+    echo"Name is not empty";
+}
+echo PHP_EOL;
+
+if (isset($name)&& (is_numeric($n) || $n != '')) {
+    echo "Name is set and it's not empty";
+}else{
+    echo"Name is not set or it's empty";
+}
