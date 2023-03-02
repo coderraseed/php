0659ab887a7bdf6138972c0d2a0ab997943072db
Md Rasedul Islam <coderraseed@gmail.com>
2023-03-02 22:28:28 +0600

    M2 practice finished

--- m2/prerecordclass/class19.php
+++ m2/prerecordclass/class19.php
@@ -1,1 +1,20 @@
-This is file 19
+<?php 
+$n = 10;
+$result = ($n%2==0) ? "A": (($n%2==1) ? "B" : "C");
+
+echo $result;
+
+echo PHP_EOL;
+$result = (($n%2==0) ? "A": ($n%2==1)) ? "B" : "C";
+echo $result;
+
+
+echo PHP_EOL;
+$tuitionFee = 20000;
+$discount1 = ($tuitionFee / 100) * 25;
+$discount2 = ($tuitionFee / 100) * 20;
+$discount3 = ($tuitionFee / 100) * 15;
+
+$commCltr = ($tuitionFee >= 20000) ? "25% commission \${$discount1}" : (($tuitionFee >= 10000) ? "20% commission \${$discount2}" : (($tuitionFee >= 7000) ? "15% commission \${$discount3}" : "Not applicable for commission"));
+
+echo $commCltr;
