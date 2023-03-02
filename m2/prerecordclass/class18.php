d57ab867888e72af52b13dbfabe230e03b628b53
Md Rasedul Islam <coderraseed@gmail.com>
2023-03-02 22:12:39 +0600

    Alternative Syntax

--- m2/prerecordclass/class18.php
+++ m2/prerecordclass/class18.php
@@ -1,1 +1,70 @@
-This is file 18
+<?php 
+//Control structure
+
+$n = 12;
+if ( $n % 2 == 0 ) {
+    echo "{$n} is an even number";
+} else {
+    echo "{$n} is an odd number ";
+}
+
+echo PHP_EOL;
+echo "=============\n";
+// Alternative syntax of previous code
+
+if ( $n % 2 == 0 ):
+    echo "{$n} is an even number";
+    echo PHP_EOL;
+else:
+    echo "{$n} is an odd number";
+    echo PHP_EOL;
+endif;
+echo PHP_EOL;
+
+
+echo "=============\n";
+// Alternative syntax of previous code
+
+if ( $n % 2 == 0 ):
+    ?>
+This is an even number";
+    
+    <?php
+else:
+    ?>
+This is an odd number";
+    
+    <?php
+endif;
+echo PHP_EOL;
+
+
+
+echo "=============\n";
+// Alternative syntax of previous code
+
+if ( $n % 2 == 0 ){
+    ?>
+This is an even number";
+    
+    <?php
+}else{
+    ?>
+This is an odd number";
+    
+    <?php
+}
+
+
+echo PHP_EOL;
+
+
+echo "=============\n";
+//In switch case
+switch($n % 2 == 0 ):
+    case 0:
+        echo "{$n} is an even number";
+        break;
+    default:
+        echo "{$n} is an odd number";
+endswitch;
