9bbccec23bab18f76d7dfa32f1bea3351ee0542b
Md Rasedul Islam <coderraseed@gmail.com>
2023-03-02 19:35:59 +0600

    small project of if condition

--- m2/prerecordclass/class11.php
+++ m2/prerecordclass/class11.php
@@ -1,14 +1,27 @@
 <?php
-$fruits = ['apple', 'banana', 'mango', 'plum', 'orange', 'straberry'];
-print_r($fruits);
-$newFruits = ['black Berry', 'jakfruite'];
-$fruits2 = array_splice($fruits, 2 );
-print_r($fruits2);
-print_r($fruits);
-$fruits3 = array_splice($fruits, 1, 5, $newFruits );
-print_r($fruits3);
-//print_r(array_splice($fruits, 2,2));
-//print_r(array_splice($fruits, -5));
-//print_r(array_splice($fruits, -5,-1));
-//print_r(array_splice($fruits, -3,-1, true));
+//small project by if..elseif..else condition
 
+//1. Dividable by 4?
+//2. Dividable by 100?
+//3. Dividable by 400?
+$year=1900;
+//This is a readable code for any php coder
+if ($year%4==0&& $year%100==0 && $year%400 ==0) {
+    echo "{$year} is a leap year";
+} elseif ($year%4==0 && $year%100 == 0) {
+    echo "{$year} is a leap year";
+}elseif ($year%4==0) {
+    echo "{$year} is a leap year";
+} else {
+    echo "{$year} is not a leap year";
+}
+
+echo PHP_EOL;
+
+//This condition and upper condition same purpose
+//This is a smart code but not easy to readable code for any php coder
+if ($year % 4 == 0 && ($year % 100 != 0 || $year % 400 != 0)) {
+    echo "{$year} is a leap year";
+}else {
+    echo "{$year} is not a leap year";
+}
