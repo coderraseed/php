3deb39534d167701a3800d8f34c1fe8600218069
Md Rasedul Islam <coderraseed@gmail.com>
2023-03-06 21:36:07 +0600

    Strign class strated

--- m4/prerecordclass/string/class21.php
+++ m4/prerecordclass/string/class21.php
@@ -1,1 +1,34 @@
 <?php
+//!In PHP, a string is a sequence of characters that is used to represent textual data. A string can contain letters, numbers, symbols, and even whitespace. In PHP, strings are defined using double quotes or single quotes.
+
+
+$name = "Rashed";
+
+$string = "My name is $name\n";
+echo $string;
+$string = "My name is $name\n\t new date\n";
+echo $string;
+
+//This name is now doc because using single quotation
+$nowDoc = <<<'EOD'
+My name
+is {$name}
+I read 
+PHP,
+laravel &
+wordpress
+EOD;
+
+echo $nowDoc;
+
+echo "\n============this is now doc============\n";
+
+$hearDoc = <<<EOD
+My name
+is {$name}
+I read 
+PHP,
+laravel &
+wordpress
+EOD;
+
--- m4/prerecordclass/string/class22.php
+++ m4/prerecordclass/string/class22.php
@@ -1,1 +1,21 @@
 <?php
+//!=========ASCII=============
+//?ASCII stands for American Standard Code for Information Interchange. It is a widely used character encoding standard that assigns unique numerical codes to represent characters and symbols in computers and other electronic devices.
+
+//?The ASCII standard defines a set of 128 characters, including letters, numbers, punctuation marks, and control codes. Each character is assigned a unique 7-bit binary code, which means that there are 128 possible values that can be represented.
+
+$ascii_code = 65;
+$character  = chr( $ascii_code );
+echo $character . "\n"; // Output: A
+
+$character  = 'C';
+$ascii_code = ord( $character );
+echo $ascii_code . "\n"; // Output: 67
+//===================================
+$ascii_code         = 65;
+$hex_representation = bin2hex( chr( $ascii_code ) );
+echo $hex_representation . "\n"; // Output: 41
+
+$hex_representation = "41";
+$character          = hex2bin( $hex_representation );
+echo $character . "\n"; // Output: A
