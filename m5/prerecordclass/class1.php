94eb526c1729ae120fce4fe5dbe5521c877f6792
Md Rasedul Islam <coderraseed@gmail.com>
2023-03-07 06:46:16 +0600

    OOP revision class started

--- m5/prerecordclass/class1.php
+++ m5/prerecordclass/class1.php
@@ -1,6 +1,22 @@
 <?php 
 
-//OOP in php
+//!OOP in php
+
+/* 
+Object-Oriented Programming (OOP) is a programming paradigm that allows developers to organize code into reusable and modular components called objects. PHP is a popular programming language that supports OOP concepts. Here are some key features of OOP in PHP:
+
+    Classes: Classes are the blueprints for creating objects in PHP. They define properties (variables) and methods (functions) that can be used to manipulate the object's state and behavior.
+
+    Objects: Objects are instances of a class. They have their own set of properties and methods that can be accessed and modified through the object.
+
+    Encapsulation: Encapsulation is the process of hiding the internal details of an object from the outside world. This is achieved in PHP by using access modifiers like public, private, and protected to control the visibility of class members.
+
+    Inheritance: Inheritance allows developers to create new classes that inherit the properties and methods of existing classes. This enables code reuse and can help to reduce code duplication.
+
+    Polymorphism: Polymorphism is the ability of an object to take on many forms. In PHP, this can be achieved through method overriding and method overloading.
+
+    Abstraction: Abstraction is the process of focusing on the essential features of an object while ignoring the details that are not relevant. This is achieved in PHP by using abstract classes and interfaces.
+*/
 
 //How to write a class in php, Given bellow
 class Person{
@@ -25,3 +41,6 @@ class Person{
 //How to write an object, is given bellow
 $human = new Person; // 
 echo $human->AddNumber(1,2);
+
+
+// git add file_name; git commit -m ""; git push origin master
