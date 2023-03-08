568e2474c130b11388004c5870e7bfa5226f0eec
Md Rasedul Islam <coderraseed@gmail.com>
2023-03-08 22:30:42 +0600

    What Happend

--- m5/prerecordclass/class11.php
+++ m5/prerecordclass/class11.php
@@ -1,1 +1,28 @@
-This is file 11
+<?php 
+//! Step one
+class Shape{}
+class Shapes {
+    private $shapes;
+    public function __construct(){
+        $this->shapes=array();
+    }
+    public function addShape($shape){
+       array_push($this->shapes, $shape);
+    }
+    public function totalShapes(){
+       return count($this->shapes);
+    }
+}
+
+class Rectangle extends Shape {}
+class TriAngle extends Shape{}
+class Student {}
+
+
+$r =new Ractangle();
+$t =new TriAngle();
+
+$shapesCollection = new Shapes();
+$shapesCollection->addShape(new Ractangle());
+$shapesCollection->addShape(new TriAngle());
+$shapesCollection->addShape(new Student());
