edf9dfcad8db2574106edc1a74c051d11032d675
Md Rasedul Islam <coderraseed@gmail.com>
2023-03-08 17:39:37 +0600

    Some small project of inheritanc

--- m5/prerecordclass/class7.php
+++ m5/prerecordclass/class7.php
@@ -1,1 +1,40 @@
-This is file 7
+<?php
+
+//! If same method in Parent class and child class that time only run child class method\
+//? Let's see the example bellow
+/* class ParentClass{
+protected $name;
+public function __construct($name){
+$this->sayHi();
+}
+public function sayHi(){
+echo "Hi from {$this->name}";
+}
+}
+class ChildClass extends ParentClass{
+function sayHi(){
+echo "Hello";
+}
+}
+
+$cc = new ChildClass("ABCD"); */
+
+//! If we want to run same method parent class and child class we should follow the follow systems
+class ParentClass {
+    protected $name;
+    public function __construct( $name ) {
+        $this->name = $name;
+        $this->sayHi();
+    }
+    public function sayHi() {
+        echo "Hi from {$this->name}\n";
+    }
+}
+class ChildClass extends ParentClass {
+    function sayHi() {
+        parent::sayHi();
+        echo "Hello\n";
+    }
+}
+
+$cc = new ChildClass( "ABCD" );
