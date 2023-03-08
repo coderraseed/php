29ebc036b57fdc29c359a8540cb43f2af01d66dd
Md Rasedul Islam <coderraseed@gmail.com>
2023-03-08 09:19:55 +0600

    How to write an object class method and property

--- m5/prerecordclass/class2.php
+++ m5/prerecordclass/class2.php
@@ -1,22 +1,121 @@
 <?php
-class Human {
+//!Step One
+/* class Human{
+    public function sayHi(){
+        echo "Assalamualaikim";
+    }
+}
+class Cat{
+    public function sayHi(){
+        echo "Meow";
+    }
+}
+class Dog{
+    public function sayHi(){
+        echo "Woof";
+    }
+}
+
+$h1= new Human();
+$c1 =new Cat();
+$d1 =new Dog();
+
+echo $h1->sayHi() . PHP_EOL; 
+echo $c1->sayHi(). PHP_EOL;
+echo $d1->sayHi(). PHP_EOL; */
+
+//!Step two (Get & Set value)
+/* 
+class Human{
+
     public $name;
-
-    public function __construct( $humanName ) {
-        echo "New Human object is created\n";
-        $this->name = $humanName;
+    public function sayHi(){
+        echo "Assalamualaikim";
     }
-
-    public function sayHi() {
-        echo "Assalamu Alaikum";
-        $this->sayName();
-    }
-
-    public function sayName() {
-        echo "My name is {$this->name}\n";
-    }
-    
     
 }
-$h1 = new Human( "Rashed" );
-$h1->sayHi();
+
+
+$h1= new Human();
+$h1->name= "Hasin";//Set
+
+$h1->sayHi(); 
+echo $h1->name. PHP_EOL;//get */
+//!Step Three (Get & Set value)
+/* 
+class Human{
+
+    public $name;
+    public function sayHi(){
+        echo "Assalamualaikim";
+    }
+    public function sayName(){
+        echo "My name is {$this->name}";
+    }
+    
+}
+
+
+$h1= new Human();
+$h1->name= "Hasin";//Set
+$h2= new Human();
+$h2->name= "Haider";//Set
+
+$h1->sayHi(); 
+$h2->sayName();  */
+
+
+
+//!Step four (Get & Set value)
+/* 
+class Human{
+
+    public $name;
+    public function sayHi(){
+        echo "Assalamualaikim \n";
+        $this->sayName();
+    }
+    public function sayName(){
+        echo "My name is {$this->name} \n";
+    }
+    
+}
+
+
+$h1= new Human();
+$h1->name= "Hasin";//Set
+$h2= new Human();
+$h2->name= "Haider";//Set
+
+$h1->sayHi(); 
+$h2->sayName(); 
+
+ */
+
+
+//!Step five (private)
+
+class Human{
+
+    public $name;
+    public function sayHi(){
+        echo "Assalamualaikim \n";
+        $this->sayName();
+    }
+    private function sayName(){
+        echo "My name is {$this->name} \n";
+    }
+    
+}
+
+
+$h1= new Human();
+$h1->name= "Hasin";//Set
+$h2= new Human();
+$h2->name= "Haider";//Set
+
+$h1->sayHi(); 
+//$h2->sayName(); 
+
+
+
