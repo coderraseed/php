61300cd4d84055047a934c87c9c025729f6e1955
Md Rasedul Islam <coderraseed@gmail.com>
2023-03-08 22:12:26 +0600

    Use of final keyword in abstract class

--- m5/prerecordclass/class10.php
+++ m5/prerecordclass/class10.php
@@ -1,1 +1,31 @@
-This is file 10
+<?php 
+abstract class OurClass{
+    public function doSomeThing(){
+        echo "Doing Something";
+    }
+}
+
+Class MyClass extends OurClass{
+    function doSomeThing(){
+        echo "Doing Nothing\n";
+    }
+}
+
+$mc = new MyClass();
+$mc ->doSomeThing(); 
+
+//! If we declared a method by "final" key this function can't override
+
+abstract class OurName{
+    final function yourName(){
+        echo "Doing Something\n";
+    }
+}
+
+Class MyName extends OurName{
+    /* function yourName(){
+        echo "Doing Nothing\n"; 
+}*/
+}
+$md = new MyName();
+$md ->yourName();
