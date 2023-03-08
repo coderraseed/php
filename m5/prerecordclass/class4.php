1d78fff45fef64bc540c73c1106f23cfc35e777f
Md Rasedul Islam <coderraseed@gmail.com>
2023-03-08 09:24:13 +0600

    How to write public & private methode

--- m5/prerecordclass/class4.php
+++ m5/prerecordclass/class4.php
@@ -1,1 +1,29 @@
-This is file 4
+<?php
+class Fund {
+    //public $fund;
+    private $fund;
+    private $purpose;
+    function __construct( $initialFund = 0 ) {
+        $this->fund = $initialFund;
+    }
+    public function addFund( $money, $purpose=null ) {
+        $this->fund += $money;
+        $this->purpose= $purpose;
+    }
+    public function deductFund( $money, $purpose=null ) {
+        $this->fund -= $money;
+        $this->purpose= $purpose;
+
+    }
+    public function getTotal() {
+        echo "{$this->purpose} Total fund is {$this->fund}\n";
+    }
+
+}
+$outFund = new Fund( 100 );
+//$outFund->fund=75;
+$outFund->getTotal();
+$outFund->addFund( 10 , "After adding money");
+$outFund->getTotal();
+$outFund->deductFund( 5 );
+$outFund->getTotal();
